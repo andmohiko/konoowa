517c9ba606c971d765d7b34ebd80d5fc352182db
andmohiko <andmohiko@gmail.com>
2023-09-16 01:15:52 +0900

    feat: fetch news from microcms

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,14 +1,38 @@
+import axios from 'axios'
+
 import type { NextPage } from 'next'
 
 import { NewsContainer } from '~/components/Containers/NewsContainer'
 import { DefaultLayout } from '~/components/Layouts/DefaultLayout'
+import { Article } from '~/types/article'
 
-const News: NextPage = () => {
+type Props = {
+  articles: Array<Article>
+}
+
+const News: NextPage<Props> = ({ articles }: Props) => {
   return (
     <DefaultLayout>
-      <NewsContainer />
+      <NewsContainer articles={articles} />
     </DefaultLayout>
   )
 }
 
 export default News
+
+export async function getStaticProps() {
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_MICROCMS_ENDPOINT}/news`,
+    {
+      headers: {
+        'X-API-KEY': process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
+      },
+    },
+  )
+
+  return {
+    props: {
+      articles: data.contents,
+    },
+  }
+}
