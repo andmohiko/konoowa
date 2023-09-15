7535dfcbb305f663653408bc3bc1abfd2d06698d
andmohiko <andmohiko@gmail.com>
2023-09-15 18:53:08 +0900

    feat: add header and booter

--- src/components/Navigations/BottomFooter/index.tsx
+++ src/components/Navigations/BottomFooter/index.tsx
@@ -1,20 +1,41 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 import styles from './style.module.scss'
 
 export const BottomFooter = (): React.ReactElement => {
   return (
     <footer className={styles.footer}>
-      <p className={styles.logo}>この世の終わりゲーミング</p>
-      <ul className={styles.menu}>
-        <li className={styles.menuItem}>ABOUT</li>
-        <li className={styles.menuItem}>NEWS</li>
-        <li className={styles.menuItem}>MEMBERS</li>
-        <li className={styles.menuItem}>SPONSERS</li>
-        <li className={styles.menuItem}>CONTACT</li>
-      </ul>
-      <div className={styles.copyright}>
-        <p>
-          <small>© 2023 @andmohiko</small>
-        </p>
+      <Image
+        src="/images/logo.png"
+        alt="この世の終わりゲーミング"
+        width={120}
+        height={120}
+        className={styles.logo}
+      />
+      <div className={styles.footerContent}>
+        <ul className={styles.menu}>
+          <li className={styles.menuItem}>
+            <Link href="/about">ABOUT</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/news">NEWS</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/members">MEMBERS</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/sponsors">SPONSORS</Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/contact">CONTACT</Link>
+          </li>
+        </ul>
+        <div className={styles.copyright}>
+          <p>
+            <small>© 2023 @andmohiko</small>
+          </p>
+        </div>
       </div>
     </footer>
   )
