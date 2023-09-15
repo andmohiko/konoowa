27900957ca82c73aa09231c86bad0ba1b6aee835
andmohiko <andmohiko@gmail.com>
2023-09-15 22:03:04 +0900

    feat: add ogp

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -8,5 +8,24 @@ export const PageHead = (): React.ReactElement => (
       content="ゲーミングチーム「この世の終わりゲーミング」の公式サイトです"
     />
     <link rel="icon" href="/favicon.ico" />
+
+    <link rel="manifest" href="/site.webmanifest" />
+    <meta name="msapplication-TileColor" content="#2f245c" />
+    <meta name="theme-color" content="#2f245c" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@andmohiko" />
+
+    <meta property="og:url" content={'https://konoowa.vercel.app/'} />
+    <meta property="og:title" content={`この世の終わりゲーミング`} />
+    <meta
+      property="og:description"
+      content={'ゲーミングチーム「この世の終わりゲーミング」の公式サイトです'}
+    />
+    <meta property="og:site_name" content="この世の終わりゲーミング" />
+    <meta
+      property="og:image"
+      content={'https://konoowa.vercel.app/images/konoowa.png'}
+    />
   </Head>
 )
